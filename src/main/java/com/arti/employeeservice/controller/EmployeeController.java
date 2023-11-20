2d8dca2264a0fb1654aaa8466ebbf8647535edeb
Artem Ivanov <arti.ivnv@gmail.com>
2023-11-20 17:48:41 +0200

    more endpoints and not found handler

--- src/main/java/com/arti/employeeservice/controller/EmployeeController.java
+++ src/main/java/com/arti/employeeservice/controller/EmployeeController.java
@@ -1,6 +1,11 @@
 package com.arti.employeeservice.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +26,28 @@ public class EmployeeController {
     }
 
     @GetMapping
-    public List<Employee> all(){
+    public List<Employee> allEmployees(){
         return service.all();
     }
+
+    @PostMapping
+    public void addEmployee(@RequestBody Employee newEmployee){
+        service.add(newEmployee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee oneEmployee(@PathVariable Long id){
+        return service.one(id);
+    }
+
+    @PutMapping
+    public Employee replaceEmployee(@RequestBody Employee newEmployee, Long id){
+        return service.replaceEmployee(newEmployee, id);
+    }
+
+    @DeleteMapping
+    public void deleteEmployee(Long id){
+        service.delete(id);
+    }
+
 }
--- src/main/java/com/arti/employeeservice/service/EmployeeService.java
+++ src/main/java/com/arti/employeeservice/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.arti.employeeservice.service;
 
 import org.springframework.stereotype.Service;
 
+import com.arti.employeeservice.exceptions.EmployeeNotFoundException;
 import com.arti.employeeservice.model.Employee;
 import com.arti.employeeservice.repository.EmployeeRepository;
 
@@ -19,6 +20,30 @@ public class EmployeeService {
 
     public List<Employee> all(){
         return repository.findAll();
+    }
+
+    public Employee one(Long id){
+        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    }
+
+    public void add(Employee employee){
+        repository.save(employee);
+    }
+
+    public Employee replaceEmployee(Employee newEmployee, Long id){
+        return repository.findById(id).map(employee -> {
+            employee.setName(newEmployee.getName());
+            employee.setRole(newEmployee.getRole());
+            return repository.save(employee);
+        })
+        .orElseGet(() -> {
+            newEmployee.setId(id);
+            return repository.save(newEmployee);
+        });
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
     }
 
 }
